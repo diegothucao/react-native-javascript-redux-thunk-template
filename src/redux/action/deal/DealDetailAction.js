36317f6aa8aa5bea3b8b65b146d0dc6a73667013
Thu CAO <cao.trung.thu@gmail.com>
2019-04-17 14:03:34 +0700

    add business to reducer

--- src/redux/action/deal/DealDetailAction.js
+++ src/redux/action/deal/DealDetailAction.js
@@ -2,15 +2,16 @@ import C from '../../../util/constants'
 import ajax from '../../../util/ajax'
 
 export function fetchDetail(deal) {
-	return async (dispatch, getState)  => {
+	// return async (dispatch, getState)  => {
+	return async (dispatch, _)  => {
 		try {
-			let { dealDetailData } =  getState()
-			if (dealDetailData.deal === undefined || dealDetailData.deal.key !== deal.key) {
+	//		let { dealDetailData } =  getState()
+	//	if (dealDetailData.deal === undefined || dealDetailData.deal.key !== deal.key) {
 				dispatch({
 					type: C.INITIAL_DETAIL,
 					payload: deal
 				})
-			}
+	//	}
 			dispatch({
 				type: C.DEAL_FETCH_DETAIL,
 				payload: await ajax.fetchDealDetail(deal.key)
--- src/redux/reducer/deal/DealDetailReducer.js
+++ src/redux/reducer/deal/DealDetailReducer.js
@@ -10,8 +10,8 @@ export default dealDetailReducer = (state = initialState, action) => {
     switch (action.type) {
             case C.INITIAL_DETAIL:
                     return {
-                            ...state,
-                            deal: action.payload
+                        ...state,
+                         deal: state.deal === undefined || state.deal.key !== action.payload.key ? action.payload: state.deal
                     }
             case C.DEAL_FETCH_DETAIL:
                     return {
